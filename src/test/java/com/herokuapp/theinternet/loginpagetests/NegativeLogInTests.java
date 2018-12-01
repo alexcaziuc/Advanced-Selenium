a9bee0b3b841f3520fec911bf3401c1d39320a02
alexcaziuc <alexcaziuc@gmail.com>
2018-12-01 22:24:38 +0200

    Initial commit

---
+++ src/test/java/com/herokuapp/theinternet/loginpagetests/NegativeLogInTests.java
@@ -0,0 +1,38 @@
+package com.herokuapp.theinternet.loginpagetests;
+
+import com.herokuapp.theinternet.base.TestUtilities;
+import com.herokuapp.theinternet.pages.LoginPage;
+import com.herokuapp.theinternet.pages.SecureAreaPage;
+import com.herokuapp.theinternet.pages.WelcomePageObject;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class NegativeLogInTests extends TestUtilities {
+
+	@Parameters({ "username", "password", "expectedMessage" })
+	@Test(priority = 1)
+	public void negativeTest(String username, String password, String expectedErrorMessage) {
+		log.info("Starting negativeTest");
+
+		// open main page
+		WelcomePageObject welcomePage = new WelcomePageObject(driver, log);
+		welcomePage.openPage();
+
+		// Click on Form Authentication link
+		LoginPage loginPage = welcomePage.clickFormAuthenticationLink();
+
+		// enter username and password
+		// execute negative log in
+		loginPage.negativeLogIn(username, password);
+
+		// wait for error message
+		loginPage.waitForErrorMessage();
+		String message = loginPage.getErrorMessageText();
+
+		// Verification
+		Assert.assertTrue(message.contains(expectedErrorMessage), "Message doesn't contain expected text.");
+	}
+
+}
