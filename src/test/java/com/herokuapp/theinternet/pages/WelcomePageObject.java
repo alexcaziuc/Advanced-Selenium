4579a8236c7f319b265599f51fa5b39a0ba370fd
alexcaziuc <alexcaziuc@gmail.com>
2018-12-04 21:30:03 +0200

    Alerts Tests

--- src/test/java/com/herokuapp/theinternet/pages/WelcomePageObject.java
+++ src/test/java/com/herokuapp/theinternet/pages/WelcomePageObject.java
@@ -11,6 +11,7 @@ public class WelcomePageObject extends BasePageObject{
     private By formAuthenticationLinkLocator = By.linkText("Form Authentication");
     private By checkboxesLinkLocator = By.linkText("Checkboxes");
     private By dropdownLinkLocator = By.linkText("Dropdown");
+    private By javaScriptAlertsLinkLocator = By.linkText("JavaScript Alerts");
 
     public WelcomePageObject(WebDriver driver, Logger log) {
         super(driver, log);
@@ -46,5 +47,14 @@ public class WelcomePageObject extends BasePageObject{
         log.info("Clicking Dropdown link on Welcome Page");
         click(dropdownLinkLocator);
         return new DropdownPage(driver, log);
+    }
+
+    /**
+     * Open JavaScriptAlertsPage by clicking on Javascript Alerts Link
+     */
+    public JavaScriptAlertsPage clickJavaScriptAlertsLink() {
+        log.info("Clicking JavaScript Alerts link on Welcome Page");
+        click(javaScriptAlertsLinkLocator);
+        return new JavaScriptAlertsPage(driver, log);
     }
 }
